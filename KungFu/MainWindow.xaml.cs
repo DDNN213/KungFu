c9529c95e7c4f03dce67f9ee00c276f3efb376b3
柏星 <hongzhilong123456@163.com>
2015-08-26 20:12:38 +0800

    Ver 1.0 (Build 2015/08/26) WPF Basics 20:12 Hongzhilong

---
+++ KungFu/MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Globalization;
+using System.IO;
+using Microsoft.Kinect;
+
+namespace KungFu
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window, INotifyPropertyChanged
+    {
+        private readonly int bytesPerPixel = (PixelFormats.Bgr32.BitsPerPixel + 7) / 8;
+
+        private KinectSensor kinectSensor = null;
+
+        private CoordinateMapper coordinateMapper = null;
+
+        private MultiSourceFrameReader multiFrameSourceReader = null;
+
+        private uint bitmapBackBufferSize = 0;
+
+        private DepthSpacePoint[] colorMappedToDepthPoints = null;
+
+        public MainWindow() {
+
+            this.kinectSensor = KinectSensor.GetDefault();
+
+            this.multiFrameSourceReader = this.kinectSensor.OpenMultiSourceFrameReader(FrameSourceTypes.Depth | FrameSourceTypes.Color | FrameSourceTypes.BodyIndex);
+
+            this.coordinateMapper = this.kinectSensor.CoordinateMapper;
+
+
+
+
+            InitializeComponent();
+        }
+    }
+}
